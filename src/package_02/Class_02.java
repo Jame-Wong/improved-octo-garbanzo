2d47389fcc0a8ab0fc3b629e7ad18defe422f6fc
Jame-Wong <583438674@qq.com>
2017-12-05 00:22:38 +0800

    add a new class: package_02.Class02.java

---
+++ src/package_02/Class_02.java
@@ -0,0 +1,8 @@
+package package_02;
+
+public class Class_02 {
+	public void method_01() {
+		// TODO Auto-generated method stub
+		System.out.println("class: package_02.Class_02.method_01()"); 
+	}
+}
