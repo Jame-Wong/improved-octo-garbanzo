3385a90f82706751340b1677e0de750904c20eed
Jame-Wong <583438674@qq.com>
2017-12-04 11:26:53 +0800

    commit in the first time
    Change-Id: I659229895a916c922915b613c13fe4e51e61a9bd
    Signed-off-by: Jame-Wong <583438674@qq.com>

---
+++ src/package_01/Class_01.java
@@ -0,0 +1,13 @@
+package package_01;
+
+public class Class_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("class: package_01.Class_01.main()");
+		
+		Class_02 c2 = new Class_02();
+		c2.method_01(); 
+	}
+
+}
---
+++ src/package_01/Class_02.java
@@ -0,0 +1,11 @@
+package package_01;
+
+public class Class_02 {
+
+	public void method_01() {
+		// TODO Auto-generated method stub
+		System.out.println("class: package_01.Class_02.method_02()"); 
+	}
+
+}
+ 
