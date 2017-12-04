9d65dbf4a9131321cf7ccae286dca7860ca6f143
Jame-Wong <583438674@qq.com>
2017-12-04 11:29:22 +0800

    commit in the third time

---
+++ src/package_01/Class_03.java
@@ -0,0 +1,10 @@
+package package_01;
+
+public class Class_03 {
+	
+	public void method_01() {
+		// TODO Auto-generated method stub
+		System.out.println("class: package_01.Class_03.method_01()"); 
+	}
+
+}
